2bf806ca4f8ad1dd4e2877441665c9705e807a94
rodartes <rodartes@oregonstate.edu>
2021-02-25 16:16:26 +0000

    Initial commit

---
+++ test_fibonacci.py
@@ -0,0 +1,13 @@
+import unittest
+import fibonacci
+
+class TestSeq(unittest.TestCase):
+  def testInit(self):
+    self.assertEqual(fibonacci.fun(0), 0)
+  def testSum(self):
+    self.assertEqual(fibonacci.fun(1)+fibonacci.fun(2), 2)
+  def testSeq(self):
+    self.assertEqual(fibonacci.fun(10), 34)
+
+if __name__ == '__main__':
+  unittest.main()
